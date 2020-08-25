69a87c78682948b0cd774574d5cfe2f5b0af1cb7
Chris LoJacono <clojacono92@gmail.com>
2020-08-25 09:58:19 -0500

    put a modal in the info button, now for data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
 import {dinos, adventures} from "./../assets/data/dinoData.js"
-
+import { infoModal } from "./components/infoModal.js"
 
 
 const addDinos = (array, div) => {
+    
     $(`#${div}`).html('')
     array.forEach((item, index)=> {
          $(`#${div}`).append(`<div class="card" style="width: 18rem;">
@@ -14,18 +15,41 @@ const addDinos = (array, div) => {
             <div class="progress-bar" style="width: ${item.health}%" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
         <div class="button-section">
-
-            <a href="#" id="pet-${index}" class="btn btn-outline-primary">Pet</a>
-            <a href="#" id="feed-${index}" class="btn btn-outline-success">Feed</a>
-            <a href="#" id="adventure-${index}"class="btn btn-outline-warning">Adventure</a>
-            <a href="#" id="delete-${index}" class="btn btn-outline-danger">Delete</a>
+            <button type="button" id="pet-${item.id}" class="btn btn-outline-primary">Pet</button>
+            <button type="button" id="feed-${item.id}" class="btn btn-outline-success">Feed</button>
+            <button type="button" id="adventure-${item.id}" class="btn btn-outline-warning">Adventure</button>
+            <button type="button" id="delete-${item.id}" class="btn btn-outline-danger">Delete</button>
+            <button type="button" id="info-${item.id}" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModal">
+            Info
+            </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>
             </div>
             </div>
         </div>`)
-}
-    )}
+})
+
+    }
 
 const init = ()=>{
     addDinos(dinos, 'kennel');
+    
 }
 init()
