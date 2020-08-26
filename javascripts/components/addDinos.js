3b1441e2fd23e84bbb0bc1c177cbe9bb81ed300c
Chris LoJacono <clojacono92@gmail.com>
2020-08-26 13:56:35 -0500

    delete button working

--- javascripts/components/addDinos.js
+++ javascripts/components/addDinos.js
@@ -1,73 +1,6 @@
-import { infoModal } from "./infoModal.js";
-import { divSorter } from "./divSort.js";
-import {
-  graveyard,
-  hospital,
-  kennel,
-  dinos,
-} from "../../assets/data/dinoData.js";
 
-const cardEvents = (array, index, item) => {
-  // FEED\
-  let bool = true;
-  $(`#feed-${item.id}`).on("click", () => {
-    if (array[index].health < 100) {
-      array[index].health += 10;
-      $(`#progressBar-${item.id}`).html(``);
-      $(`#progressBar-${item.id}`).html(
-        ` <div class="progress-bar progress-bar-striped ${
-          item.health > 50 ? "bg-success" : "bg-warning"
-        } progress-bar-animated" style="width: ${
-          item.health
-        }%" id="progress-${index}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"><p class="health-status">${
-          item.health
-        }%</p></div>`
-      );
-      if (array === hospital && array[index].health > 50 && bool === true) {
-        kennel.push(item);
-        $(`#card-${item.id}`).remove();
-        $("#kennel").append(singleDino(item));
-        bool = false;
-      }
-      if (array === graveyard && array[index].health > 1 && bool === true) {
-        hospital.push(item);
-        $(`#card-${item.id}`).remove();
-        $("#hospital").append(singleDino(item));
-        bool = false;
-      }
-    }
-  });
-  
-  // PET
-  $(`#pet-${item.id}`).on("click", () => {
-    if (array[index].health < 100) {
-      array[index].health += 5;
-      $(`#progressBar-${item.id}`).html(``);
-      $(`#progressBar-${item.id}`).html(
-        ` <div class="progress-bar progress-bar-striped ${
-          item.health > 50 ? "bg-success" : "bg-warning"
-        } progress-bar-animated" style="width: ${
-          item.health
-        }%" id="progress-${index}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"><p class="health-status">${
-          item.health
-        }%</p></div>`
-      );
+import { cardEvents } from './cardButtons.js'
 
-      if (array === hospital && array[index].health > 50 && bool === true) {
-        kennel.push(item);
-        $(`#card-${item.id}`).remove();
-        $("#kennel").append(singleDino(item));
-        bool = false;
-      }
-      if (array === graveyard && array[index].health > 1 && bool === true) {
-        hospital.push(item);
-        $(`#card-${item.id}`).remove();
-        $("#hospital").append(singleDino(item));
-        bool = false;
-      }
-    }
-  });
-};
 
 const singleDino = (item) => {
   let dinoString = `<div class="card" id="card-${
@@ -163,4 +96,4 @@ const addDinos = (array, div) => {
   });
 };
 
-export { addDinos };
+export { addDinos, singleDino };
